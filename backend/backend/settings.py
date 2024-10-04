5ea78839242786a4a2df8bb57da2dd37da554f37
Andry925 <andry925@ukr.net>
2024-10-04 14:45:59 +0300

    chore: add accounts model, add custom user model

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'debug_toolbar',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -94,6 +95,8 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
+
+AUTH_USER_MODEL = 'accounts.User'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
