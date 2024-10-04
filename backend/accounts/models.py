61d319da96646eeb39d5ccfab2c7547094417dd5
Andry925 <andry925@ukr.net>
2024-10-04 14:44:53 +0300

    feat: implement different multi-user authentication via proxy models

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,5 +1,7 @@
-from django.contrib.auth.models import BaseUserManager, AbstractUser
+from django.contrib.auth.models import BaseUserManager, AbstractUser, PermissionsMixin
 from django.db import models
+
+from .utils import parse_university_data
 
 
 class UserManager(BaseUserManager):
@@ -16,25 +18,30 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
-        if not (extra_fields.get('is_staff')
-                and not extra_fields.get('is_superuser')):
-            raise ValueError(
-                'Superuser must have is_staff=True and is_superuser=True')
+        if not (extra_fields.get('is_staff') and extra_fields.get('is_superuser')):
+            raise ValueError('Superuser must have is_staff=True and is_superuser=True')
         return self.create_user(email, password, **extra_fields)
 
 
-class User(AbstractUser):
+class User(AbstractUser, PermissionsMixin):
     class Roles(models.TextChoices):
         STUDENT = 'STUDENT', 'Student'
         TUTOR = 'TUTOR', 'Tutor'
 
-    role = models.CharField(
-        max_length=20,
-        choices=Roles.choices,
-        default=Roles.STUDENT)
-    name = models.CharField(max_length=255, required=True, unique=True)
-    email = models.EmailField(max_length=255, required=True, unique=True)
+    class CurrentGrade(models.TextChoices):
+        FIRST = 'FIRST', 'First year'
+        SECOND = 'SECOND', 'Second year'
+        THIRD = 'THIRD', 'Third year'
+        FOURTH = 'FOURTH', 'Fourth year'
+        POSTGRADUATE = 'POSTGRADUATE', 'Postgraduate'
+
+    role = models.CharField(max_length=20, choices=Roles.choices, default=Roles.STUDENT)
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    phone_number = models.CharField(max_length=20, unique=True)
     is_active = models.BooleanField(default=True)
+    current_grade = models.CharField(max_length=20, choices=CurrentGrade.choices, default=CurrentGrade.FIRST)
+    university = models.CharField(max_length=255, choices=parse_university_data())
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -47,3 +54,67 @@ class User(AbstractUser):
 
     def __str__(self):
         return f'{self.email}'
+
+
+class StudentManager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(*args, **kwargs).filter(role=User.Roles.STUDENT)
+
+
+class TutorManager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(*args, **kwargs).filter(role=User.Roles.TUTOR)
+
+
+class StudentMore(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='students_more')
+
+
+class Student(User):
+    objects = StudentManager()
+    base_role = User.Roles.STUDENT
+
+    @property
+    def more(self):
+        return self.students_more
+
+    class Meta:
+        proxy = True
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.role = self.base_role
+        return super().save(*args, **kwargs)
+
+
+def upload_to(instance, filename):
+    return f'images/{filename}'
+
+
+def file_upload_to(instance, filename):
+    return f'confirmation_files/{filename}'
+
+
+class TutorMore(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='tutors_more')
+    students = models.ManyToManyField(Student, blank=True)
+    is_approved = models.BooleanField(default=False)
+    photo_url = models.ImageField(upload_to=upload_to)
+    confirmation_file = models.FileField(upload_to=file_upload_to)
+
+
+class Tutor(User):
+    objects = TutorManager()
+    base_role = User.Roles.TUTOR
+
+    @property
+    def more(self):
+        return self.tutors_more
+
+    class Meta:
+        proxy = True
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.role = self.base_role
+        return super().save(*args, **kwargs)
