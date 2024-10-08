0e1abcbab859b91415a0c545b6c462e75d10b481
Andry925 <andry925@ukr.net>
2024-10-08 14:19:46 +0300

    feat: implement registration endpoint

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import permissions, status
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class UserRegistrationView(APIView):
+    permission_classes = [permissions.AllowAny, ]
+    parser_classes = [MultiPartParser, FormParser]
+
+    def post(self, request):
+        serializer = UserSerializer(
+            data=request.data, context={
+                'request': request})
+        if not (serializer.is_valid(raise_exception=True)
+                and not request.user.is_authenticated):
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST)
+        user = serializer.create(request.data)
+        if user:
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
