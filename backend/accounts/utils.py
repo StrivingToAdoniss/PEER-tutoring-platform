610296d32b0634851d6817a9c93356df0cb167a6
Andry925 <andry925@ukr.net>
2024-10-11 14:07:46 +0300

    fix: make components of accounts app more human-readable

--- backend/accounts/utils.py
+++ backend/accounts/utils.py
@@ -4,4 +4,5 @@ import json
 def parse_university_data():
     with open('accounts/data/universities.json', 'r') as json_file:
         data = json.load(json_file)
-        return [(university, university) for university in data.get("universities", [])]
+        return [(university, university)
+                for university in data.get("universities", [])]
