09eec20cb46c774e98ad41d670c299d84193934f
Andry925 <andry925@ukr.net>
2024-12-06 14:06:14 +0200

    feat: implement endpoints for tutor profile

--- backend/tutors_profiles/views.py
+++ backend/tutors_profiles/views.py
@@ -1,5 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, permissions
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.parsers import MultiPartParser, FormParser
 
@@ -22,7 +23,39 @@ class TutorListView(generics.ListAPIView):
 
     def get_queryset(self):
         return (
-            TutorProfile.objects.filter(tutor__is_approved=False)
+            TutorProfile.objects.filter(tutor__is_approved=True)
             .prefetch_related('tutor')
             .order_by('price_per_hour', 'id')
         )
+
+
+class TutorProtectedListView(generics.CreateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = TutorProfileSerializer
+    pagination_class = TutorCustomPagination
+    parser_classes = [MultiPartParser, FormParser]
+
+    def get_queryset(self):
+        queryset = TutorProfile.objects.filter(tutor=self.request.user.tutors_more).prefetch_related('tutor')
+        return queryset
+
+    def perform_create(self, serializer):
+        if not hasattr(self.request.user, 'tutors_more'):
+            raise PermissionDenied("Only tutors can create a tutor profile.")
+        serializer.save(tutor=self.request.user.tutors_more)
+
+
+class TutorProtectedUpdateView(generics.RetrieveUpdateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = TutorProfileSerializer
+    pagination_class = TutorCustomPagination
+    parser_classes = [MultiPartParser, FormParser]
+
+    def get_queryset(self):
+        queryset = TutorProfile.objects.filter(tutor=self.request.user.tutors_more).prefetch_related('tutor')
+        return queryset
+
+    def perform_update(self, serializer):
+        if not hasattr(self.request.user, 'tutors_more'):
+            raise PermissionDenied("Only tutors can create a tutor profile.")
+        serializer.save(tutor=self.request.user.tutors_more)
