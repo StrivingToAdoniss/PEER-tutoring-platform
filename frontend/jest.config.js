aa2c91158b4d0cf34baba00909719b5fb46bc4d9
StrivingToAdoniss <gorovyi.sv@gmail.com>
2024-12-12 11:02:23 +0200

    Update jest.config.js

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -7,6 +7,9 @@ module.exports = {
         "\\.(css|less|scss|sass)$": "identity-obj-proxy",
         "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
         "axios": "axios/dist/node/axios.cjs"
+      },
+      "scripts": {
+        "test": "jest"
       }
   };
   
