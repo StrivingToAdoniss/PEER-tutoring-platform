3870ff4cf2e0ef9fab361c5a55edbc270bfe590d
StrivingToAdoniss <gorovyi.sv@gmail.com>
2024-12-12 11:34:48 +0200

    Update tests

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -7,15 +7,6 @@ module.exports = {
         "\\.(css|less|scss|sass)$": "identity-obj-proxy",
         "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
         "axios": "axios/dist/node/axios.cjs"
-      },
-      "scripts": {
-        "test": "jest"
-      },
-      preset: 'ts-jest',
-      testEnvironment: 'node',
-      moduleNameMapper: {
-        '^@src/(.*)$': '<rootDir>/src/$1'
-      },
-      testMatch: ['<rootDir>/src/**/*.test.ts']
+      }
   };
   
